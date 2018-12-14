407f5e32001c33169d0f616398d22b6760eb4a5b
Junichi Ito <jit@sonicgarden.jp>
2018-12-14 21:45:25 +0900

    Drop users and blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_121731) do
+ActiveRecord::Schema.define(version: 2018_12_14_124350) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -19,28 +19,12 @@ ActiveRecord::Schema.define(version: 2018_12_14_121731) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_blogs_on_user_id"
-  end
-
   create_table "books", force: :cascade do |t|
     t.integer "author_id"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
