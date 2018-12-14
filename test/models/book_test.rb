8ded092ecb42f3b6d65bcfdf0ff0dbae3ee7dc01
Junichi Ito <jit@sonicgarden.jp>
2018-12-15 08:43:44 +0900

    Refine test

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -28,6 +28,8 @@ class BookTest < ActiveSupport::TestCase
   end
 
   test ".order_by_param" do
+    assert_equal [@bob_book, @alice_book, @allen_book], Book.order_by_param("books.title DESC")
+
     # https://rails-sqli.org/
     sql = <<~SQL
       CASE WHEN SUBSTR(books.author_email, 1, 1) = 'x' THEN 0 ELSE 1 END
@@ -40,5 +42,24 @@ class BookTest < ActiveSupport::TestCase
       CASE WHEN SUBSTR(books.author_email, 1, 1) = 'x' THEN 0 ELSE 1 END
     SQL
     assert_equal [@bob_book, @alice_book, @allen_book], Book.order_by_param_with_arel_sql(sql)
+  end
+
+  ALLOWED_ATTRIBUTES = %w(title author_name).freeze
+  def validate_order_by(text)
+    unless text.in?(ALLOWED_ATTRIBUTES)
+      raise ArgumentError, "Unexpected attribute: #{text}"
+    end
+    text
+  end
+
+  test '.validate_order_by' do
+    text = 'title'
+    order_by = validate_order_by(text)
+    assert_equal [@allen_book, @alice_book, @bob_book], Book.order(Arel.sql(order_by))
+
+    text = 'author_email'
+    assert_raises(ArgumentError) do
+      validate_order_by(text)
+    end
   end
 end
