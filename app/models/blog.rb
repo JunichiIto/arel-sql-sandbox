c31897b3f8482ea068c8d6f6806b59fdd5163dd1
Junichi Ito <jit@sonicgarden.jp>
2018-12-14 09:33:40 +0900

    Add order by examples

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,9 @@
 class Blog < ApplicationRecord
+  scope :order_by_title, -> { order(:title) }
+  scope :order_by_title_desc, -> { order(title: :desc) }
+  scope :order_by_title_sql, -> { order("blogs.title") }
+  scope :order_by_title_length, -> { order("LENGTH(blogs.title)") }
+  scope :order_by_title_arel_sql, -> { order(Arel.sql("LENGTH(blogs.title)")) }
+  scope :order_by_param, -> (param) { order(param) }
+  scope :order_by_param_with_arel_sql, -> (param) { order(Arel.sql(param)) }
 end
--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class BlogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".order_by_title" do
+    assert_equal [blogs(:blahhh), blogs(:hi)], Blog.order_by_title
+  end
+
+  test ".order_by_title_desc" do
+    assert_equal [blogs(:hi), blogs(:blahhh)], Blog.order_by_title_desc
+  end
+
+  test ".order_by_title_sql" do
+    assert_equal [blogs(:blahhh), blogs(:hi)], Blog.order_by_title_sql
+  end
+
+  test ".order_by_title_length" do
+    assert_equal [blogs(:hi), blogs(:blahhh)], Blog.order_by_title_length
+  end
+
+  test ".order_by_title_arel_sql" do
+    assert_equal [blogs(:hi), blogs(:blahhh)], Blog.order_by_title_arel_sql
+  end
+
+  test ".order_by_param" do
+    assert_equal [blogs(:blahhh), blogs(:hi)], Blog.order_by_param("blogs.title")
+  end
+
+  test ".order_by_param_with_arel_sql" do
+    assert_equal [blogs(:blahhh), blogs(:hi)], Blog.order_by_param_with_arel_sql("blogs.title")
+  end
 end
