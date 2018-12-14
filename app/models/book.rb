acc2989446058f5086b1c1599dbe823a33622798
Junichi Ito <jit@sonicgarden.jp>
2018-12-15 07:37:03 +0900

    Refine test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
-  scope :order_by_symbol, -> { order(:author_name) }
-  scope :order_by_symbol_desc, -> { order(author_name: :desc) }
-  scope :order_by_text, -> { order("books.author_name") }
-  scope :order_by_function, -> { order("LOWER(books.author_name)") }
-  scope :order_by_function_with_arel_sql, -> { order(Arel.sql("LOWER(books.author_name)")) }
+  scope :order_by_symbol, -> { order(:title) }
+  scope :order_by_symbol_desc, -> { order(title: :desc) }
+  scope :order_by_text, -> { order("books.title") }
+  scope :order_by_function, -> { order("LOWER(books.title)") }
+  scope :order_by_function_with_arel_sql, -> { order(Arel.sql("LOWER(books.title)")) }
   scope :order_by_param, -> (param) { order(param) }
   scope :order_by_param_with_arel_sql, -> (param) { order(Arel.sql(param)) }
 end
